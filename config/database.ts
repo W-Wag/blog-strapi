235841d21849ccc4c4bcf9b88edcc0bf9ed518e3
Wagner De Oliveira Martins <wagner.oliv.m@gmail.com>
2023-12-04 16:05:14 -0300

    Update database.ts

--- config/database.ts
+++ config/database.ts
@@ -50,10 +50,10 @@ export default ({ env }) => {
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD', 'strapi'),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
