a634847ff87cb1d02217bd977b2ca8a103aecb6f
Wagner De Oliveira Martins <wagner.oliv.m@gmail.com>
2023-12-12 14:44:04 -0300

    Ativando o método count no strapi

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -4,4 +4,9 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::post.post');
+export default factories.createCoreController('api::post.post', {
+  count(ctx) {
+    var { query } = ctx.request
+    return strapi.query('api::post.post').count({ where: query })
+  }
+});
